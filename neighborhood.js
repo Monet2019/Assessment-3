250a163221d4603a8eeedceb647195a12b477547
Shekerah Henderson <kerahende@gmail.com>
2023-07-28 12:29:46 -0500

    Done

--- neighborhood.js
+++ neighborhood.js
@@ -1,12 +1,12 @@
-
 const randomRestaurant = () => {
-  const restaurants = ["The Hungry Robot", "The Daily Grind", "The Park Cafe"];
+  const restaurants = ["The Grind Burger Bar", "Adriatica Village", "Layered"];
   const randomIndex = Math.floor(Math.random() * restaurants.length);
   return restaurants[randomIndex];
 };
 
 const button = document.querySelector("#random-restaurant");
+
 button.addEventListener("click", function() {
   const restaurant = randomRestaurant();
-  alert(`Here's a random restaurant recommendation for you: ${restaurant}`);
+  document.getElementById("random-restaurant").innerHTML = `Here's a random restaurant recommendation for you: ${restaurant}`;
 });
